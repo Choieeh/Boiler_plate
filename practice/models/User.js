21a08163e31e55f4dc34e6aac480ea56ea6ec903
Choieeh <chyh0182@naver.com>
2021-02-07 14:39:16 +0000

    password bcrypt

--- practice/models/User.js
+++ practice/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
 	name: {
@@ -30,6 +32,24 @@ const userSchema = mongoose.Schema({
 		//token 유효기간 token은 유효성 검사
 		type: Number
 	}
+})
+
+userSchema.pre('save', function( next ){
+	var user = this;
+	//this는 userSchema를 가리킴
+	
+	if(user.isModified('password'))
+		{//비밀번호 암호화
+		//salt를 이용해서 비밀번호를 암호화하게 됨. saltRound는 salt가 몇글자인지.
+		bcrypt.genSalt(saltRounds, function(err, salt) {
+			if(err) return next(err)
+
+			bcrypt.hash(user.password, salt, function(err, hash){
+				if(err) return next(err)
+				user.password = hash
+				next()
+			})
+		});}
 })
 
 const User = mongoose.model('User', userSchema)

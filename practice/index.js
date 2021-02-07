013134e191859da60ea6107c425dda774c2d975d
Choieeh <chyh0182@naver.com>
2021-02-07 14:18:46 +0000

    add secure

--- practice/index.js
+++ practice/index.js
@@ -4,13 +4,16 @@ const port = 5000
 const bodyParser = require('body-parser');
 const {User} = require("./models/User");
 
+const config = require('./config/key')
+//key에서 config를 받아온 후 mongoURI를 연결하도록 한다.
+
 //application url 형식
 app.use(bodyParser.urlencoded({extended:true}));
 //application json 형식 가져오기
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Ehyun:asdf1234@cluster0.yqvbz.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 	useNewUrlParser: true, useUnifiedTopology : true, useCreateIndex:true, useFindAndModify : false
 }).then(() => console.log('MongoDB connect'))
   .catch(err => console.log(err))

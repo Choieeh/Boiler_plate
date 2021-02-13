47a5857beb6c81e1adcf61b36b459a21d4dec4ca
Choieeh <chyh0182@naver.com>
2021-02-13 11:28:40 +0000

    redux

--- practice/client/src/index.js
+++ practice/client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'antd/dist/antd.css';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<Provider store>
+		<App />
+	</Provider>
+  ,
   document.getElementById('root')
 );
 
--- practice/client/src/setupProxy.js
+++ practice/client/src/setupProxy.js
@@ -1,9 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
 module.exports = function(app) {
 	app.use(
 		'/api',
-			proxy({
-			target : 'https://practice-react-asdf.run.goorm.io/',
+		createProxyMiddleware({
+			target : 'https://practice-react-asdf.run.goorm.io',
 			changeOrigin: true,
 		})
 	);

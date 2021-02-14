9391b09c7a0eabd079bc5d68d58f916301599304
Choieeh <chyh0182@naver.com>
2021-02-14 07:38:47 +0000

    make mainpage

--- practice/client/src/index.js
+++ practice/client/src/index.js
@@ -6,7 +6,7 @@ import 'antd/dist/antd.css';
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
 import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk;'
+import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)

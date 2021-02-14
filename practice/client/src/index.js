b6fc3d2c0891798cadbbb5270f3d22139f7f975f
Choieeh <chyh0182@naver.com>
2021-02-14 05:51:55 +0000

    setting redux

--- practice/client/src/index.js
+++ practice/client/src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
 import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk;'
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+//store 생성
 
 ReactDOM.render(
-	<Provider store>
+	<Provider store = {createStoreWithMiddleware(Reducer,
+			window.__REDUX_DEVTOOLS_EXTENSION__ &&
+			window.__REDUX_DEVTOOLS_EXTENSION__()
+		)}>
 		<App />
 	</Provider>
   ,
